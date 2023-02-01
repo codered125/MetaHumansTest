4128bbf8c39368fe5f23ad3bc6b16e1f4272411b
Moses <mosesflava@gmail.com>
2023-02-01 10:24:03 +0000

    Created UE5 Project

---
+++ Source/MetaHumansTest/MetaHumansTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetaHumansTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMetaHumansTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMetaHumansTestGameMode();
+};
+
+
+
